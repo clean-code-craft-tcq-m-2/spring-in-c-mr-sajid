f7b6a3e587a7a1048e0a792f2a82f997205c0f21
mr-sajid <57176354+mr-sajid@users.noreply.github.com>
2022-10-14 10:24:00 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,11 +1,16 @@
 #include "stats.h"
 #include "alerts.h"
 
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+/*
+* @brief    To calculate the Average, min and max values of the given array.
+* @param    numberset : float type array pointer.
+            setlength : array length.
+* @return   Stats : result to be stored and return in structure type.
+*/
 struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    struct Stats s = {0.0};
     
     if(setlength == 0)
     {
@@ -39,9 +44,6 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     
     return s;
 }
-
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
